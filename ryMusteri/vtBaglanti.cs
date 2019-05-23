4e52c716a4d5909dae69c31d6248eace7464f3e5
Mert Can Altın <mertcanaltin@posta.mu.edu.tr>
2019-05-23 18:20:46 +0300

    Add files via upload

---
+++ ryMusteri/vtBaglanti.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ryMusteri
+{
+    public class vtBaglanti
+    {
+        public static string vtAdres = "Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=" 
+            + Environment.CurrentDirectory + "\\data\\musteri.mdf;Integrated Security = True;";
+            //"Data Source=localhost;Initial Catalog=musteri;" +"Integrated Security=true;";
+
+        //public static string vtAdres = "Data Source=localhost;Initial Catalog=musteri;" +
+        //    "User ID=sa;Password=mmyo;";
+    }
+}
---
+++ ryMusteri/vtIslem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ryMusteri
+{
+    public class vtIslem
+    {
+        #region Tanımlama
+         static SqlConnection musConn = new SqlConnection(vtBaglanti.vtAdres);
+         static SqlDataAdapter musAdp;
+         static SqlCommand musCmd = new SqlCommand();
+        #endregion
+        #region Kullanıcı Tanımlı Metod
+        public static DataTable veriGetir(string sorgu)
+        {
+            DataTable musTablo = new DataTable();
+            musTablo.Clear();
+            musAdp = new SqlDataAdapter(sorgu, musConn);
+            musAdp.Fill(musTablo);
+            return musTablo;
+        }
+
+        public static void komutCalistir(string sorgu)
+        {
+            try
+            {
+                if (musConn.State == ConnectionState.Closed)
+                    musConn.Open();
+                musCmd.Connection = musConn;
+                musCmd.CommandText = sorgu;
+                musCmd.ExecuteNonQuery();
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.ToString(), "Bilgi",
+            MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            finally
+            {
+                musConn.Close();
+            }
+        }
+        #endregion
+    }
+}
